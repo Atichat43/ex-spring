2ada0de903438509cd4aff1326cc2d951a885f4f
Atichat Lappanopakon <boss_atln@outlook.com>
2019-08-29 08:39:09 +0700

    Logging

--- src/main/java/com/atichat/demo/DemoApplication.java
+++ src/main/java/com/atichat/demo/DemoApplication.java
@@ -10,29 +10,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.CommandLineRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 @RestController
-public class DemoApplication extends SpringBootServletInitializer implements CommandLineRunner{
+public class DemoApplication extends SpringBootServletInitializer implements CommandLineRunner {
 	@Value("${spring.application.name:defaultValue}")
-  private String name;
+	private String name;
+	private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 
 	@Override
-   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-      return application.sources(DemoApplication.class);
-   }
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DemoApplication.class);
+	}
 
 	public static void main(String[] args) {
+		logger.info("this is a info message");
+		logger.warn("this is a warn message");
+		logger.error("this is a error message");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
 	@Override
 	public void run(String... arg0) throws Exception {
-		 System.out.println("Hello world from Command Line Runner");
+		System.out.println("Hello world from Command Line Runner");
 	}
 
 	@RequestMapping(value = "/")
-   public String name() {
-      return name;
-   }
+	public String name() {
+		return name;
+	}
 }
