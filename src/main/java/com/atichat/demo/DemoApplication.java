9a72ce286183814324b14865d0c888bea4ba2ecf
Atichat Lappanopakon <boss_atln@outlook.com>
2019-08-29 08:03:43 +0700

    Runnner

--- src/main/java/com/atichat/demo/DemoApplication.java
+++ src/main/java/com/atichat/demo/DemoApplication.java
@@ -1,20 +1,29 @@
 package com.atichat.demo;
 
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.CommandLineRunner;
 
 @SpringBootApplication
 @RestController
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer implements CommandLineRunner{
+
+	@Override
+   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+      return application.sources(DemoApplication.class);
+   }
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-	
-	@RequestMapping(value = "/")
-   public String hello() {
-      return "Hello World";
-   }
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		 System.out.println("Hello world from Command Line Runner");
+	}
 }
