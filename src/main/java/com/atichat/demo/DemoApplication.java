64dc8d106d60f5e7c9d44857235bcd73cd108296
Atichat Lappanopakon <boss_atln@outlook.com>
2019-08-29 08:17:41 +0700

    Application properties

--- src/main/java/com/atichat/demo/DemoApplication.java
+++ src/main/java/com/atichat/demo/DemoApplication.java
@@ -1,10 +1,12 @@
 package com.atichat.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.CommandLineRunner;
@@ -12,6 +14,8 @@ import org.springframework.boot.CommandLineRunner;
 @SpringBootApplication
 @RestController
 public class DemoApplication extends SpringBootServletInitializer implements CommandLineRunner{
+	@Value("${spring.application.name:defaultValue}")
+  private String name;
 
 	@Override
    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
@@ -26,4 +30,9 @@ public class DemoApplication extends SpringBootServletInitializer implements Com
 	public void run(String... arg0) throws Exception {
 		 System.out.println("Hello world from Command Line Runner");
 	}
+
+	@RequestMapping(value = "/")
+   public String name() {
+      return name;
+   }
 }
